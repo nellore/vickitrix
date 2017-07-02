2e7f7951a88b62bd4b03571a45ecfbe399704c09
Abhi Nellore <anellore@gmail.com>
2017-07-02 11:38:55 -0700

    breaks vickitrix but starts adding security features and such

---
+++ vickirules.py
@@ -0,0 +1,16 @@
+rules = [
+    {
+        'handle' : 'vickicryptobot',
+        'action' : 'buy',
+        'product' : 'ETH-USD',
+        'funds' : '{available}',
+        'condition' : '"ETHUSD" in {tweet} and "long" in {tweet}'
+    },
+    {
+        'handle' : 'vickicryptobot',
+        'action' : 'sell',
+        'product' : 'ETH-USD',
+        'size' : '{available}',
+        'condition' : '"ETHUSD" in {tweet} and "short" in {tweet}'
+    },
+]
