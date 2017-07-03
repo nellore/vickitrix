52dda348ac333543b592cc59352b8434743d0f99
Abhi Nellore <anellore@gmail.com>
2017-07-02 22:33:06 -0700

    adds missing commas to sentiment.py

--- vickitrix/rules/sentiment.py
+++ vickitrix/rules/sentiment.py
@@ -3,14 +3,14 @@ rules = [
         'keywords' : ['ethereum', 'good'],
         'action' : 'buy',
         'product' : 'ETH-USD',
-        'funds' : '{available}*0.001'
+        'funds' : '{available}*0.001',
         'condition' : '"good" in {tweet} and "ethereum" in {tweet}'
     },
     {
         'keywords' : ['ethereum', 'bad'],
         'action' : 'sell',
         'product' : 'ETH-USD',
-        'size' : '{available}*0.001'
+        'size' : '{available}*0.001',
         'condition' : '"bad" in {tweet} and "ethereum" in {tweet}'
     }
 ]
